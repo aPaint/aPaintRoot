9762a300117a907ee304c81519de9bd941552ade
apaint <apaint.adm@gmail.com>
2020-08-29 19:50:30 +0800

    Updates

--- script.js
+++ script.js
@@ -13,9 +13,19 @@ dButton.addEventListener('mouseup',()=>{
 })
 
 
+
+
 function countClick(){
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.countapi.xyz/hit/apaint.io/530bf04d-c821-46b9-9810-897f35b19984");
     xhr.responseType = "json";
     xhr.send();
 }
+
+
+
+
+
+
+
+
